c56b0879d1c73f7dbfc8ec7b9c517a8c20b18962
Joshua Netterfield <joshua@nettek.ca>
2012-07-28 11:56:30 -0400

    Add licence.

--- bridge/bridgesettings.cpp
+++ bridge/bridgesettings.cpp
@@ -1,3 +1,12 @@
+/*******************************************************
+
+    Part of the Creator Live Music Production Suite.
+Copyright (C) Joshua Netterfield <joshua@nettek.ca> 2012
+
+                  All rights reserved.
+
+*******************************************************/
+
 #include "bridgesettings.h"
 #include "ui_bridgesettings.h"
 
--- bridge/bridgesettings.h
+++ bridge/bridgesettings.h
@@ -1,3 +1,12 @@
+/*******************************************************
+
+    Part of the Creator Live Music Production Suite.
+Copyright (C) Joshua Netterfield <joshua@nettek.ca> 2012
+
+                  All rights reserved.
+
+*******************************************************/
+
 #ifndef BRIDGESETTINGS_H
 #define BRIDGESETTINGS_H
 
